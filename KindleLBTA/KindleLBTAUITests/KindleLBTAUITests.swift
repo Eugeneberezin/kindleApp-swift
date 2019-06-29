0df691555b102ff6262511928b05f6fceb169c46
Eugene Berezin <berezin.nlp@gmail.com>
2019-06-29 15:37:52 -0700

    adding IDs and first test.

--- KindleLBTA/KindleLBTAUITests/KindleLBTAUITests.swift
+++ KindleLBTA/KindleLBTAUITests/KindleLBTAUITests.swift
@@ -11,22 +11,24 @@ import XCTest
 class KindleLBTAUITests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        
         continueAfterFailure = false
 
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    
         XCUIApplication().launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
 
     func testExample() {
+        let bookTitle = ["Steve Jobs", "Bill Gates: A Biography"]
+        
+        XCTAssert(XCUIApplication().tables.staticTexts[bookTitle[0]].exists, "Steve Jobs is not displayed")
+        XCTAssert(XCUIApplication().tables.staticTexts[bookTitle[1]].exists, "Bill Gates: A Biography")
+        
         
     }
 
